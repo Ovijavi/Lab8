e2761caaa3295a300ed8313ba72e534ef25da75c
Ovijavi <galvanjavier540@gmail.com>
2021-05-23 00:38:37 -0700

    finished part 1

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -29,7 +29,8 @@ describe('Basic user flow for SPA ', () => {
 
   it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
     // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
-
+    page.click('journal-entry');
+    window.location.href.includes("/#entry1");
   });
 
   it('Test4: On first Entry page - checking page header title', async () => {
--- __tests__/router.test.js
+++ __tests__/router.test.js
@@ -1,4 +1,44 @@
 /**
  * @jest-environment jsdom
  */
+// import { router } from '../scripts/router.js';
+import { pushToHistory } from '../scripts/router.js';
 
+describe('settings tests', () => {
+    test('test if setting state is pushed to history', () => {
+        let returnedHistory = pushToHistory('settings', 0);
+        expect(returnedHistory.state).toEqual({ page: "settings" });
+    });
+
+    test('test if history is correct length', () => {
+        let currLength = history.length;
+        let returnedHistory = pushToHistory('settings', 0);
+        expect(returnedHistory.length).toBe(currLength + 1);
+    });
+});
+
+describe('entry tests', () => {
+    test('test if entry state is pushed to history', () => {
+        let returnedHistory = pushToHistory('entry', 0);
+        expect(returnedHistory.state).toEqual({ page: `entry${0}` });
+    });
+
+    test('test if history is correct length', () => {
+        let currLength = history.length;
+        let returnedHistory = pushToHistory('entry', 0);
+        expect(returnedHistory.length).toBe(currLength + 1);
+    });
+});
+
+describe('default tests', () => {
+    test('test if default state is pushed to history', () => {
+        let returnedHistory = pushToHistory('', 0);
+        expect(returnedHistory.state).toEqual({});
+    });
+
+    test('test if history is correct length', () => {
+        let currLength = history.length;
+        let returnedHistory = pushToHistory('', 0);
+        expect(returnedHistory.length).toBe(currLength + 1);
+    });
+});
