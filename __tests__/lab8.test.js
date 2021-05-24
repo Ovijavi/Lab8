be4e79be81ce809f63250be480aca588b9ca798d
Ovijavi <galvanjavier540@gmail.com>
2021-05-23 23:03:50 -0700

    finished part 2

--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -29,13 +29,17 @@ describe('Basic user flow for SPA ', () => {
 
   it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
     // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
-    page.click('journal-entry');
-    window.location.href.includes("/#entry1");
+    let pageEntry = await page.$('journal-entry');
+    await pageEntry.click();
+    await page.waitForTimeout(100);
+    expect(page.url()).toContain("/#entry1");
   });
 
   it('Test4: On first Entry page - checking page header title', async () => {
     // implement test4: Clicking on the first journal entry should update the header text to “Entry 1” 
-
+    let header = await page.$('h1');
+    let headerProp = await header.getProperty('innerText');
+    expect(headerProp._remoteObject['value']).toContain('Entry 1');
   });
 
   it('Test5: On first Entry page - checking <entry-page> contents', async () => {
@@ -51,58 +55,139 @@ describe('Basic user flow for SPA ', () => {
           }
         }
       */
+     let entryPage = await page.$('entry-page');
+
+     let entry = await entryPage.getProperty('entry');
+     let entryJson = await entry.jsonValue();
+     expect(entryJson).toEqual({ 
+      title: 'You like jazz?',
+      date: '4/25/2021',
+      content: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.",
+      image: {
+        src: 'https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455',
+        alt: 'bee with sunglasses'
+      }
+    });
 
   }, 10000);
 
   it('Test6: On first Entry page - checking <body> element classes', async () => {
     // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
-
+    let body = await page.$('body');
+    let bodyremote = await body._remoteObject;
+    expect(bodyremote.description).toContain('single-entry');
   });
 
   it('Test7: Clicking the settings icon, new URL should contain #settings', async () => {
     // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
-
+    let settingIcon = await page.$('img');
+    await settingIcon.click();
+    await page.waitForTimeout(100);
+    expect(page.url()).toContain("/#settings");
   });
 
   it('Test8: On Settings page - checking page header title', async () => {
     // implement test8: Clicking on the settings icon should update the header to be “Settings”
-
+    let header = await page.$('h1');
+    let headerProp = await header.getProperty('innerText');
+    expect(headerProp._remoteObject['value']).toContain('Settings');
   });
 
   it('Test9: On Settings page - checking <body> element classes', async () => {
     // implement test9: Clicking on the settings icon should update the class attribute of <body> to ‘settings’
-
+    let body = await page.$('body');
+    let bodyremote = await body._remoteObject;
+    expect(bodyremote.description).toContain('settings');
   });
 
   it('Test10: Clicking the back button, new URL should be /#entry1', async() => {
     // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
-
+    await page.goBack();
+    expect(page.url()).toContain("/#entry1");
   });
 
   // define and implement test11: Clicking the back button once should bring the user back to the home page
-
+  it('Test11: Clicking the back button once should bring the user back to the home page', async() => {
+    await page.goBack();
+    let header = await page.$('h1');
+    let headerProp = await header.getProperty('innerText');
+    expect(headerProp._remoteObject['value']).toContain('Journal Entries');
+  });
 
   // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
-
+  it('Test12: When the user if on the homepage, the header title should be “Journal Entries”', async() => {
+    let header = await page.$('h1');
+    let headerProp = await header.getProperty('innerText');
+    expect(headerProp._remoteObject['value']).toContain('Journal Entries');
+  });
 
   // define and implement test13: On the home page the <body> element should not have any class attribute 
-
+  it('Test13: On the home page the <body> element should not have any class attribute', async() => {
+    let body = await page.$('body');
+    let bodyremote = await body._remoteObject;
+    expect(bodyremote.description).toBe('body');
+  });
 
   // define and implement test14: Verify the url is correct when clicking on the second entry
-
+  it('Test14: Verify the url is correct when clicking on the second entry', async() => {
+    let pageEntry = await page.$$('journal-entry');
+    let entry = pageEntry[1];
+    await entry.click();
+    await page.waitForTimeout(100);
+    expect(page.url()).toContain("/#entry2");
+  });
 
   // define and implement test15: Verify the title is current when clicking on the second entry
-
+  it('Test15: Verify the title is current when clicking on the second entry', async() => {
+    let header = await page.$('h1');
+    let headerProp = await header.getProperty('innerText');
+    expect(headerProp._remoteObject['value']).toContain('Entry 2');
+  });
 
   // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
+  it('Test16: Verify the entry page contents is correct when clicking on the second entry', async() => {
+    let entryPage = await page.$('entry-page');
 
+    let entry = await entryPage.getProperty('entry');
+    let entryJson = await entry.jsonValue();
+     expect(entryJson).toEqual({ 
+      title: 'Run, Forrest! Run!',
+      date: '4/26/2021',
+      content: "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+      image: {
+        src: 'https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg',
+        alt: 'forrest running'
+      }
+    });
+  });
 
   // create your own test 17
+  it('Test17: Check if body class is correct', async() => {
+    let body = await page.$('body');
+    let bodyremote = await body._remoteObject;
+    expect(bodyremote.description).toContain('single-entry');
+  });
 
   // create your own test 18
+  it('Test18: Verify if clicking header goes back', async() => {
+    let header = await page.$('h1');
+    await header.click();
+    let headerProp = await header.getProperty('innerText');
+    expect(headerProp._remoteObject['value']).toContain('Journal Entries');
+  });
 
   // create your own test 19
-
+  it('Test19: Verify the url is correct when clicking on the third entry', async() => {
+    let pageEntry = await page.$$('journal-entry');
+    let entry = pageEntry[2];
+    await entry.click();
+    await page.waitForTimeout(100);
+    expect(page.url()).toContain("/#entry3");
+  });
   // create your own test 20
-  
+  it('Test20: Verify the title is current when clicking on the third entry', async() => {
+    let header = await page.$('h1');
+    let headerProp = await header.getProperty('innerText');
+    expect(headerProp._remoteObject['value']).toContain('Entry 3');
+  });
 });
